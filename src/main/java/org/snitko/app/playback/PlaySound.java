6db10b7e02c737111ce95cc5bf4198e0caaf5de3
Vadim <vadim.snitkovsky@gmail.com>
2016-02-01 03:12:49 -0500

    cleanup

--- src/main/java/org/snitko/app/playback/PlaySound.java
+++ src/main/java/org/snitko/app/playback/PlaySound.java
@@ -5,37 +5,37 @@ package org.snitko.app.playback;
  */
 
 import java.io.*;
+
 import sun.audio.*;
+
 
 /**
  * A simple Java sound file example (i.e., Java code to play a sound file).
  */
-public class PlaySound
-{
+public class PlaySound {
+    public void playWav(String soundFileToPlay) throws IOException {
+        InputStream in = new FileInputStream(soundFileToPlay);
+        play(in);
+    }
+
+    private void play(InputStream in) throws IOException {
+        // create an audiostream from the inputstream
+        AudioStream audioStream = new AudioStream(in);
+        // play the audio clip with the audioplayer class
+        AudioPlayer.player.start(audioStream);
+    }
+
+
     public static void main(String[] args)
-            throws Exception
-    {
+            throws Exception {
         // expect a sound file as args[0]
-        if (args.length != 1)
-        {
+        if (args.length != 1) {
             System.err.println("I need just one arg, the name of a sound file to play.");
             System.exit(1);
         }
         System.setProperty("java.awt.headless", "true");
 
         PlaySound playSound = new PlaySound();
-        playSound.play(args[0]);
-    }
-
-    public void play(String soundFileToPlay) throws IOException {
-        InputStream in = new FileInputStream(soundFileToPlay);
-        play(in);
-    }
-
-    public void play(InputStream in) throws IOException {
-        // create an audiostream from the inputstream
-        AudioStream audioStream = new AudioStream(in);
-        // play the audio clip with the audioplayer class
-        AudioPlayer.player.start(audioStream);
+        playSound.playWav(args[0]);
     }
 }
