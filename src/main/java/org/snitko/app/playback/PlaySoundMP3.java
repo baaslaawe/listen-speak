180f2e7605d4c45b9aa828a9a042d9cadf3fde61
Vadim <vadim.snitkovsky@gmail.com>
2016-02-01 03:27:51 -0500

    mp3 lib fix

--- src/main/java/org/snitko/app/playback/PlaySoundMP3.java
+++ src/main/java/org/snitko/app/playback/PlaySoundMP3.java
@@ -3,19 +3,19 @@ package org.snitko.app.playback;
 
 /**
  * Copyright [2015] vadim
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * <p/>
+ * <p>
  * Created: 2/1/16
  */
 
@@ -43,15 +43,15 @@ public class PlaySoundMP3 {
             final AudioFormat outFormat = getOutFormat(in.getFormat());
             final Info info = new Info(SourceDataLine.class, outFormat);
 
-            try (final SourceDataLine line =
-                         (SourceDataLine) AudioSystem.getLine(info)) {
+            try (final SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(info)) {
 
-                if (line != null) {
-                    line.open(outFormat);
-                    line.start();
-                    stream(getAudioInputStream(outFormat, in), line);
-                    line.drain();
-                    line.stop();
+                if (sourceDataLine != null) {
+                    sourceDataLine.open(outFormat);
+                    sourceDataLine.start();
+                    AudioInputStream audioInputStream = getAudioInputStream(outFormat, in);
+                    stream(audioInputStream, sourceDataLine);
+                    sourceDataLine.drain();
+                    sourceDataLine.stop();
                 }
             }
 
@@ -64,16 +64,14 @@ public class PlaySoundMP3 {
 
     private AudioFormat getOutFormat(AudioFormat inFormat) {
         final int ch = inFormat.getChannels();
-
         final float rate = inFormat.getSampleRate();
         return new AudioFormat(PCM_SIGNED, rate, 16, ch, ch * 2, rate, false);
     }
 
-    private void stream(AudioInputStream in, SourceDataLine line)
-            throws IOException {
+    private void stream(AudioInputStream in, SourceDataLine sourceDataLine) throws IOException {
         final byte[] buffer = new byte[4096];
         for (int n = 0; n != -1; n = in.read(buffer, 0, buffer.length)) {
-            line.write(buffer, 0, n);
+            sourceDataLine.write(buffer, 0, n);
         }
     }
 
