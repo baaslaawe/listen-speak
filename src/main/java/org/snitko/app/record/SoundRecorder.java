96226f4a1fadf4273d8eea214ef42f85a6758df3
Vadim <vadim.snitkovsky@gmail.com>
2016-02-01 13:55:19 -0500

    update copyright notices

--- src/main/java/org/snitko/app/record/SoundRecorder.java
+++ src/main/java/org/snitko/app/record/SoundRecorder.java
@@ -1,21 +1,27 @@
 package org.snitko.app.record;
 
 /**
- Copyright [2015] vadim
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-
- Created: 1/31/16
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) [2015] [Vadim Snitkovsky]
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 
@@ -24,10 +30,7 @@ import org.apache.commons.io.FileUtils;
 import javax.sound.sampled.*;
 import java.io.*;
 
-/**
- * * A sample program is to demonstrate how to record sound in Java
- * * author: www.codejava.net
- */
+
 public class SoundRecorder {
     /**
      * Defines an audio format.  Using standard telephony format for now
@@ -99,15 +102,13 @@ public class SoundRecorder {
         }
 
         public void update(LineEvent event) {
-            if (LineEvent.Type.START.equals(event.getType()))
-            {
+            if (LineEvent.Type.START.equals(event.getType())) {
                 System.out.println("Started Recording.  Message from the listener.");
                 try {
                     Thread.sleep(durationInSeconds * 1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
-                finally {
+                } finally {
                     System.out.println("Stopped Recording.  Message from the listener.");
                     targetDataLine.stop();
                     targetDataLine.close();
@@ -130,6 +131,6 @@ public class SoundRecorder {
         File wavFile = FileUtils.getFile(FileUtils.getUserDirectory(), "foo.wav");
         System.out.println("File Path: " + wavFile.getAbsolutePath());
         // start recording
-        recorder.record(wavFile, 5);
+        recorder.record(wavFile, 4);
     }
 }
