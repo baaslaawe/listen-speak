49ec2bac30c0f52505275914fcb80f618d2389f6
Vadim <vadim.snitkovsky@gmail.com>
2016-02-01 01:53:36 -0500

    major clean up of sleeping code for recording

--- src/main/java/org/snitko/app/record/SoundRecorder.java
+++ src/main/java/org/snitko/app/record/SoundRecorder.java
@@ -1,7 +1,21 @@
 package org.snitko.app.record;
 
 /**
- * Created by vadim on 1/31/16.
+ Copyright [2015] vadim
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ Created: 1/31/16
  */
 
 
@@ -15,14 +29,9 @@ import java.io.*;
  * * author: www.codejava.net
  */
 public class SoundRecorder {
-    // the targetDataLine from which audio data is captured
-    private TargetDataLine targetDataLine;
-
     /**
-     * Defines an audio format
+     * Defines an audio format.  Using standard telephony format for now
      */
-
-
     private AudioFormat getAudioFormat() {
         float sampleRate = 8000.0F;
         int sampleSizeInBits = 16;
@@ -38,7 +47,7 @@ public class SoundRecorder {
      */
 
 
-    public void start(File waveFile) throws LineUnavailableException, IOException {
+    public void record(File waveFile, final long durationInSeconds) throws LineUnavailableException, IOException {
 
         Mixer.Info[] mixers = AudioSystem.getMixerInfo();
         for (Mixer.Info mixer : mixers) {
@@ -52,22 +61,18 @@ public class SoundRecorder {
 
         AudioFormat audioFormat = getAudioFormat();
 
-
-        targetDataLine = AudioSystem.getTargetDataLine(audioFormat);
+        // the targetDataLine from which audio data is captured
+        TargetDataLine targetDataLine = AudioSystem.getTargetDataLine(audioFormat);
         System.out.println("targetDataLine: " + targetDataLine + "\n\tlineInfo" + targetDataLine.getLineInfo() + "\n\tformat: " + targetDataLine.getFormat());
 
 
-        /*
-        DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
-
         // checks if system supports the data targetDataLine
-        if (!AudioSystem.isLineSupported(dataLineInfo)) {
+        if (!AudioSystem.isLineSupported(targetDataLine.getLineInfo())) {
             System.out.println("Line not supported");
             System.exit(0);
         }
-        targetDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
-        */
 
+        targetDataLine.addLineListener(new TimedLineListener(targetDataLine, durationInSeconds));
         targetDataLine.open(audioFormat);
         targetDataLine.start(); // start capturing
 
@@ -83,12 +88,35 @@ public class SoundRecorder {
     /**
      * Closes the target data targetDataLine to finish capturing and recording
      */
-    public void finish() {
-        targetDataLine.stop();
-        targetDataLine.close();
-        System.out.println("Finished");
-    }
+    private class TimedLineListener implements LineListener {
 
+        private long durationInSeconds;
+        private TargetDataLine targetDataLine;
+
+        public TimedLineListener(TargetDataLine targetDataLine, long durationInSeconds) {
+            this.durationInSeconds = durationInSeconds;
+            this.targetDataLine = targetDataLine;
+        }
+
+        public void update(LineEvent event) {
+            if (LineEvent.Type.START.equals(event.getType()))
+            {
+                System.out.println("Started Recording.  Message from the listener.");
+                try {
+                    Thread.sleep(durationInSeconds * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    System.out.println("Stopped Recording.  Message from the listener.");
+                    targetDataLine.stop();
+                    targetDataLine.close();
+                    System.out.println("Finished");
+                }
+
+            }
+        }
+    }
 
     /**
      * Entry to run the program
@@ -97,29 +125,11 @@ public class SoundRecorder {
 
     public static void main(String[] args) throws Exception {
         final SoundRecorder recorder = new SoundRecorder();
-        final long recordTimeInMillie = 5 * 1000;
-
-        // creates a new thread that waits for a specified
-        // of time before stopping
-        Thread stopper = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(recordTimeInMillie);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-                recorder.finish();
-            }
-
-
-        });
-
-        stopper.start();
 
         // path of the wav file
         File wavFile = FileUtils.getFile(FileUtils.getUserDirectory(), "foo.wav");
         System.out.println("File Path: " + wavFile.getAbsolutePath());
         // start recording
-        recorder.start(wavFile);
+        recorder.record(wavFile, 3);
     }
 }
